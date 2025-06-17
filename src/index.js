e701ea5cf910238b8f63c80182e4631fe1336adc
Utkarsh <utkarshdhar05@gmail.com>
2025-06-17 23:39:13 +0530

    Add Todo Button Working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
 import './styles.css'
+
+import Todo from './todo'
+import TodoManager from './todoManager';
+import { createSpace, removeSpace } from "./space";
+
+let createTodoBtn = document.querySelector("#create-todo-btn");
+let cancelTodoBtn = document.querySelector("#cancel-todo-btn");
+let todoForm = document.querySelector("#todo-form");
+
+createTodoBtn.addEventListener("click", () => {
+    let dialog = document.querySelector("#add-todo");
+    dialog.showModal();
+});
+
+cancelTodoBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    let dialog = document.querySelector("#add-todo");
+    dialog.close();
+})
+
+function addSpace(name, color) {
+    let button = createSpace(name, color);
+    let spacesSec = document.querySelector(".spaces");
+    spacesSec.appendChild(button);
+}
+
+addSpace("personal", "blue");
+addSpace("work", "red");
+
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    let formData = new FormData(todoForm);
+    let title = formData.get("title");
+    let description = formData.get("description");
+    let space = formData.get("space");
+    let priority = formData.get("priority");
+    let dueDate = formData.get("due-date");
+
+    let todo = new Todo(title, description, dueDate, priority, space);
+    TodoManager.addTodo(todo);
+    TodoManager.save();
+
+    let contentSec = document.querySelector(".content");
+    contentSec.appendChild(todo.card);
+    let dialog = document.querySelector("#add-todo")
+    dialog.close();
+})
--- src/space.js
+++ src/space.js
@@ -1,13 +1,21 @@
 import TodoManager from "./todoManager";
 
 export function createSpace(name, color) {
+    TodoManager.addSpace(name, color);
+
+    let spaceOptions = document.querySelector("#space");
+    let newOption = document.createElement("option");
+    newOption.textContent = name;
+    newOption.value = name;
+    spaceOptions.appendChild(newOption);
+
     let button = document.createElement("button");
     let colorDiv = document.createElement("div");
     colorDiv.style.backgroundColor = color;
     colorDiv.classList.add("space-color");
 
     button.appendChild(colorDiv);
-    button.append(space);
+    button.append(name);
     button.classList.add("btn");
     button.classList.add("space-btn");
 
